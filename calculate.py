c7be157e5844be652631ae10cb7c3419bd98119f
Greg Stewart <kpb14177@uni.strath.ac.uk>
2018-03-05 19:07:02 +0000

    added missing files

---
+++ calculate.py
@@ -0,0 +1,41 @@
+'''
+Created on 20 Feb 2018
+
+@author: gregstewart96
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def calculateTime(self,aziStart,aziEnd,eleStart,eleEnd):
+        
+        if aziEnd>aziStart:
+            aziDeltaAngle = aziEnd - aziStart
+        else:
+            aziDeltaAngle = aziStart-aziEnd
+        if eleEnd>eleStart:
+            eleDeltaAngle = eleEnd - eleStart
+        else:
+            eleDeltaAngle = eleStart - eleEnd
+        if aziDeltaAngle<29:
+            aziTime = aziDeltaAngle/1.32
+        else:
+            fullSpeedTime = (aziDeltaAngle-30)/2.1
+            rampUpSlowDown = 19
+            aziTime = fullSpeedTime + rampUpSlowDown
+        if eleDeltaAngle<29:
+            eleTime = eleDeltaAngle/1.32
+        else:
+            fullSpeedTime = (eleDeltaAngle-30)/2.1
+            rampUpSlowDown = 19
+            eleTime = fullSpeedTime + rampUpSlowDown
+        if aziTime>eleTime:
+            return aziTime
+        else:
+            return eleTime
+        
+calc = MyClass()
+print(calc.calculateTime(302.070950173,68.3697402547,0,0))
+    
